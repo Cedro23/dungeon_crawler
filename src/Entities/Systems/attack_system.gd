b8836f820edd1284800bf5ad4aaa72d45dae332d
Cédric LESUEUR <clesueur@sise-plastics.com>
2024-09-02 17:44:21 +0200

    Added damage mitigation based on armor

--- src/Entities/Systems/attack_system.gd
+++ src/Entities/Systems/attack_system.gd
@@ -21,21 +21,33 @@ func attack(attacker: Entity, target: Entity) -> void:
 		if target.is_alive() == false:
 			return
 		var is_crit = _rng.randf() <= attack_component.base_crit_chance
-		var tot_damage: int = _calc_damage(attack_component, is_crit) - target_def
-	
-		_display_damage_number(tot_damage, target.position, 1.0, 10.0, is_crit)
+		var raw_dmg: int = _calc_raw_damage(attack_component, is_crit)
+		var mitigated_dmg: int = _calc_mitigated_damage(raw_dmg, target_def)
+		_display_damage_number(mitigated_dmg, target.position, 1.0, 10.0, is_crit)
 			
 		var damage_system = DamageSystem.new(attacker)
-		damage_system.apply_damage(target, tot_damage)
+		damage_system.apply_damage(target, mitigated_dmg)
 	
 
-func _calc_damage(attack_comp: AttackComponent, is_crit: bool) -> int:
+func _calc_raw_damage(attack_comp: AttackComponent, is_crit: bool) -> int:
 	var min_dmg: int = attack_comp.base_attack_damage.x
 	var max_dmg: int = attack_comp.base_attack_damage.y
 	var damage: int = _rng.randi_range(min_dmg, max_dmg)
 	if is_crit:
 		damage *= roundi(attack_comp.base_crit_multiplier)
 	return damage
+
+func _calc_mitigated_damage(raw_dmg: int, armor: int) -> int:
+	var mitigated_dmg: int
+	if armor >= 0:
+		mitigated_dmg = roundi(raw_dmg * (100.0 / (100.0 + armor)))
+	else:
+		mitigated_dmg = roundi(raw_dmg * (2 - (100.0 / (100.0 - armor))))
+
+	if mitigated_dmg < 0:
+		mitigated_dmg = 0
+
+	return mitigated_dmg
 
 func _display_damage_number(amount: int, start_pos: Vector2, duration: float, spread: float, is_crit: bool):
 	var new_scene: Node = dmg_number_scene.instantiate()
