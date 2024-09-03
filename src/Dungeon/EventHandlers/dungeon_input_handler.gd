fc1594155dc6463262072165a3989ca31e287837
Cedro23 <cedric_lesueur@outlook.fr>
2024-09-03 22:12:12 +0200

    refactored dungeon input handler

--- src/Dungeon/EventHandlers/dungeon_input_handler.gd
+++ src/Dungeon/EventHandlers/dungeon_input_handler.gd
@@ -4,11 +4,7 @@ const directions = {
 	"move_up": Vector2i.UP,
 	"move_down": Vector2i.DOWN,
 	"move_left": Vector2i.LEFT,
-	"move_right": Vector2i.RIGHT,
-	# "move_up_left": Vector2i.UP + Vector2i.LEFT,
-	# "move_up_right": Vector2i.UP + Vector2i.RIGHT,
-	# "move_down_left": Vector2i.DOWN + Vector2i.LEFT,
-	# "move_down_right": Vector2i.DOWN + Vector2i.RIGHT,
+	"move_right": Vector2i.RIGHT
 }
 
 func get_action(player: Entity) -> Action:
@@ -25,7 +21,7 @@ func get_action(player: Entity) -> Action:
 	if Input.is_action_just_pressed("wait"):
 		action = WaitAction.new(player)
 	
-	if Input.is_action_just_pressed("back"):
+	if Input.is_action_just_pressed("pause"):
 		action = EscapeAction.new(player)
 	
 	return action
